68219a4e69ac636f545a2981fb3b1b0d4c29c395
Brian Young <berkshirehunt@gmail.com>
2021-06-08 16:32:20 -0700

    initial build

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const unirest = require('unirest')
-const API_KEY = 'YOUR_API_KEY'
-const INGREDIENT_LIST = ['bananas', 'apples', 'cheese', 'crackers']
-let requestString =
-  'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/find' +
-  'ByIngredients?number=5&ranking=1&ingredients='
-const ingredientsString = INGREDIENT_LIST.map(
-  (ingredient) => ingredient + '%2C'
-)
-requestString = requestString + ingredientsString
-unirest
-  .get(requestString)
-  .header('X-RapidAPI-Key', API_KEY)
-  .end(function (result) {
-    if (result.status === 200) {
-      getRecipeData(result.body)
+const express = require("express");
+const axios = require("axios");
+const ejsLayouts = require("express-ejs-layouts");
+const methodOverride = require("method-override");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+// more info about your pokemon
+router.get("/:name", async (req, res) => {
+  try {
+    if (req.params.name) {
+      const spoonacularDataUrl = `https://api.spoonacular.com/recipes/716429/information?apiKey=bc6f5e0d736540d6821b1405b0f24d3f`;
+      const result = await axios.get(spoonacularDataUrl);
+      let response = result.data;
+console.log(response);
+      });
     }
-  })
+  } catch (err) {
+    console.log(err);
+  }
+})
+
