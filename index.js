1739ed7ed446ac2da6ae6f6721ede2a3d508f278
BeckyBoo88 <rbooco07@gmail.com>
2021-06-09 17:38:42 -0600

    added home link, form for zipcode and name

--- index.js
+++ index.js
@@ -7,14 +7,21 @@ const db = 'find_your_park'
 const app = express()
 const PORT = process.env.PORT
 const park = process.env.PARK
-console.log(park)
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use (ejsLayouts)
+app.use(express.static(__dirname + '/public'))
 
+// GET / for main homepage
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.use('/park', require('./routes/park'))
 
 
 app.listen(PORT, () => {
     console.log('...listening on', PORT );
   });
+
