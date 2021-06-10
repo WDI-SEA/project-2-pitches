3ac7d433349ee55607303aa422430c2fbe012cbb
BeckyBoo88 <rbooco07@gmail.com>
2021-06-10 11:39:45 -0600

    list page displaying properly

--- routes/park.js
+++ routes/park.js
@@ -12,18 +12,22 @@ router.post('/list', (req, res) => {
     let parkURL =`https://developer.nps.gov/api/v1/parks?stateCode=${stateCode}&sort=&api_key=${park}`
     axios.get(parkURL)
     .then((apiResponse) => {
-        console.log(apiResponse.data.data[0].fullName)
+        
         let parkName = apiResponse.data.data[0].fullName
-        let location = apiResponse.data.data[0].addresses[0]
+        let location = apiResponse.data.data[0].addresses[0].line1
+        let address = apiResponse.data.data[0].addresses[0].city
+        let address2 = apiResponse.data.data[0].addresses[0].stateCode
+        let address3 = apiResponse.data.data[0].addresses[0].postalCode
         let entranceFee = apiResponse.data.data[0].entranceFees[0].cost
         let feeDescription = apiResponse.data.data[0].entranceFees[0].description
         let hOP = apiResponse.data.data[0].operatingHours[0].description
         let hOPD = apiResponse.data.data[0].operatingHours[0].standardHours
+        console.log(parkName, location, address, address2, address3, entranceFee, feeDescription, hOP, hOPD)
 
-        res.render('list', {name: parkName, location: location, entranceFee: entranceFee, feeD: feeDescription, hours: hOP, hourD: hOPD})
+        res.render('list', {name: parkName, location: location, address: address, address2: address2, address3: address3, entranceFee: entranceFee, feeD: feeDescription, hours: hOP, hourD: hOPD})
    })
    .catch(error => console.log(error))
-})
+});
 
 // POST detail page, user can add to favorites, redirect to favorite page
 
