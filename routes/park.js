5a479777352ff46c95babc48027c53e0bf0fb7f7
BeckyBoo88 <rbooco07@gmail.com>
2021-06-09 22:15:01 -0600

    figured out how to call into long ass object

--- routes/park.js
+++ routes/park.js
@@ -9,12 +9,20 @@ const park = process.env.PARK
 router.post('/list', (req, res) => {
     let stateCode = req.body.stateCode
 
-
     let parkURL =`https://developer.nps.gov/api/v1/parks?stateCode=${stateCode}&sort=&api_key=${park}`
-    axios.get(parkURL).then(apiResponse => {
-        let parkStat = apiResponse.data.results
-        res.render('list', { parkStat })
-    })
+    axios.get(parkURL)
+    .then((apiResponse) => {
+        console.log(apiResponse.data.data[0].fullName)
+        let parkName = apiResponse.data.data[0].fullName
+        let location = apiResponse.data.data[0].addresses[0]
+        let entranceFee = apiResponse.data.data[0].entranceFees[0].cost
+        let feeDescription = apiResponse.data.data[0].entranceFees[0].description
+        let hOP = apiResponse.data.data[0].operatingHours[0].description
+        let hOPD = apiResponse.data.data[0].operatingHours[0].standardHours
+
+        res.render('list', {name: parkName, location: location, entranceFee: entranceFee, feeD: feeDescription, hours: hOP, hourD: hOPD})
+   })
+   .catch(error => console.log(error))
 })
 
 // POST detail page, user can add to favorites, redirect to favorite page
